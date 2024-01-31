70082bcb71e8dabb71382a8b58c97de97390c3d5
Eraaly <sanazera2@gmail.com>
2024-01-31 14:19:35 +0600

    A new endpoint added

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
     res.send("Express on Vercel");
 })
 
+app.get('/.well-known/assetslinks.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile(__dirname + '/assetslinks.json');
+});
+
 app.get('/generateToken', (req, res) => {
     const appId = 'bcf7d096b0e54d6d906e12879d9cec7e';
     const appCertificate = 'd58f7220740b4c92b082f1239a9063fb';
@@ -34,19 +39,6 @@ app.get('/generateToken', (req, res) => {
     res.json({ token: key });
 });
 
-app.get('/.well-known/assetslinks.json', (req, res) => {
-    res.json(
-        [{
-            "relation": ["delegate_permission/common.handle_all_urls"],
-            "target": {
-                "namespace": "android_app",
-                "package_name": "com.example.app",
-                "sha256_cert_fingerprints":
-                    ["9A:13:C2:F3:9D:66:00:FE:7A:53:A7:D2:0D:F0:A5:45:8B:E2:D2:CE:B9:30:6D:F0:15:65:BF:48:AF:CC:6D:DF"]
-            }
-        }]
-    )
-});
 
 app.get('/test', (req, res) => {
     res.json({
